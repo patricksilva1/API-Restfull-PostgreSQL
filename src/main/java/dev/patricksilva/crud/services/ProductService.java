4087224f1d2e17ce3821ef9922617633b8a4cc6f
patricksilva1 <patricksilva5160@gmail.com>
2022-10-11 17:22:27 -0300

    mvc and dto

--- src/main/java/dev/patricksilva/crud/services/ProductService.java
+++ src/main/java/dev/patricksilva/crud/services/ProductService.java
@@ -2,39 +2,78 @@ package dev.patricksilva.crud.services;
 
 import java.util.List;
 import java.util.Optional;
+import java.util.stream.Collectors;
 
+import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.rest.webmvc.support.ExceptionMessage;
 import org.springframework.stereotype.Service;
 
 import dev.patricksilva.crud.model.Product;
-import dev.patricksilva.crud.repository.ProductRepository_old;
+import dev.patricksilva.crud.model.exception.ResourceNotFoundException;
+import dev.patricksilva.crud.repository.ProductRepository;
+import dev.patricksilva.crud.shared.ProductDTO;
 
 @Service
 public class ProductService {
     @Autowired
-    private ProductRepository_old productRepository;
+    private ProductRepository productRepository;
 
-    public List<Product> findAll() {
-        return productRepository.findAll();
+    public List<ProductDTO> findAll() {
+        List<Product> products = productRepository.findAll();
+
+        return products.stream().map(product -> new ModelMapper()
+                .map(product, ProductDTO.class))
+                .collect(Collectors.toList());
+
     }
 
-    public Optional<Product> findById(Integer id) {
-        return productRepository.findById(id);
+    public Optional<ProductDTO> findById(Integer id) {
+        // Getting optional of product by id
+        Optional<Product> product = productRepository.findById(id);
+
+        if (product.isEmpty()) {
+            throw new ResourceNotFoundException(" Id:" + id + "not found!");
+        }
+        ProductDTO dto = new ModelMapper().map(product.get(), ProductDTO.class);
+        return Optional.of(dto);
     }
 
-    public Product addProduct(Product product) {
-        return productRepository.addProduct(product);
+    public ProductDTO addProduct(ProductDTO productDto) {
+        productDto.setId(null);
+
+        ModelMapper mapper = new ModelMapper();
+
+        Product product = mapper.map(productDto, Product.class);
+
+        product = productRepository.save(product);
+
+        productDto.setId(product.getId());
+
+        return productDto;
     }
 
     public void delete(Integer id) {
-        productRepository.delete(id);
+        Optional<Product> product = productRepository.findById(id);
+
+        if (product.isEmpty()) {
+            throw new ResourceNotFoundException(
+                    "Could not delete this Product with id: " + id + " product does not exist");
+        }
+        productRepository.deleteById(id);
+
     }
 
-    public Product update(Integer id, Product product) {
-        product.setId(id);
+    public ProductDTO update(Integer id, ProductDTO productDto) {
+        productDto.setId(id);
 
-        return productRepository.update(product);
+        ModelMapper mapper = new ModelMapper();
+
+        Product product = mapper.map(productDto, Product.class);
+
+        productRepository.save(product);
+
+        return productDto;
+
     }
 
 }
---
+++ src/main/java/dev/patricksilva/crud/shared/ProductDTO.java
@@ -0,0 +1,52 @@
+package dev.patricksilva.crud.shared;
+
+public class ProductDTO {
+    private Integer id;
+    private String name;
+    private Integer quantity;
+    private Double price;
+    private String observation;
+
+    // #region Getters and Setters
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Integer getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(Integer quantity) {
+        this.quantity = quantity;
+    }
+
+    public Double getPrice() {
+        return price;
+    }
+
+    public void setPrice(Double price) {
+        this.price = price;
+    }
+
+    public String getObservation() {
+        return observation;
+    }
+
+    public void setObservation(String observation) {
+        this.observation = observation;
+    }
+    // #endregion
+}
